941cc685756015921630d0d50851e8dd804cd840
KhrystynaKo <postforimportantthings@gmail.com>
2020-05-13 19:36:32 +0300

    class TodoList and method of create todo item

--- main.js
+++ main.js
@@ -5,3 +5,48 @@ class Todo {
     this.completed = completed;
   }
 }
+class TodoList {
+  constructor() {
+    this.todos = [];
+    this.render(this.todos);
+  }
+
+  render(todosArr) {
+    this.createTodo(todosArr);
+  }
+
+  createTodo(todosArr) {
+    const todoList = document.querySelector(".todo-list");
+    todoList.innerHTML = "";
+    todosArr.forEach((todo) => {
+      const liItem = document.createElement("li");
+      liItem.classList.add("li-item");
+      liItem.dataset.listId = todo.id;
+      todoList.appendChild(liItem);
+
+      const div = document.createElement("div");
+      div.className = "view";
+      liItem.appendChild(div);
+
+      const input = document.createElement("input");
+      input.className = "toggle";
+      input.setAttribute("type", "checkbox");
+      div.appendChild(input);
+
+      if (todo.completed === true) {
+        input.setAttribute("checked", true);
+        liItem.classList.add("completed");
+      }
+
+      const label = document.createElement("label");
+      label.innerHTML = todo.text;
+      div.appendChild(label);
+
+      const deleteButton = document.createElement("button");
+      deleteButton.className = "destroy";
+      div.appendChild(deleteButton);
+    });
+  }
+}
+
+const todo = new TodoList();
